ac8c395f6cd938bb6ae1b3188e6b1f47bd1e9f30
AHSanda2005 <andrianaivosonsanda@gmail.com>
2025-12-08 01:53:14 +0300

    ok

--- sql/module1-data-game.sql
+++ sql/module1-data-game.sql
@@ -692,3 +692,31 @@ INSERT INTO poste_role (id_poste, id_role, date_role) VALUES
 (22, 2, '2020-01-01'), -- Responsable Entrepôt = Manager
 (23, 3, '2020-01-01'), -- Chauffeur Poids Lourd = Employé
 (24, 2, '2020-01-01'); -- Coordinateur Logistique = Manager
+
+INSERT INTO assurance (id_assurance, nom, minpay, maxpay, taux) VALUES
+(1, 'Retenue CNaPS', NULL, NULL, 1),
+(2, 'Retenue sanitaire', NULL, NULL, 5),
+(3, 'Tranche IRSA 1', 0, 350000, 0),
+(4, 'Tranche IRSA 2', 350001, 400000, 5),
+(5, 'Tranche IRSA 3', 400001, 500000, 10),
+(6, 'Tranche IRSA 4', 500001, 600000, 15),
+(7, 'Tranche IRSA 5', 600001, 4000000, 20),
+(8, 'Tranche IRSA 6', 4000001, NULL, 25);
+
+INSERT INTO taux_heures_sup (type_heuresup, heure_debut, heure_fin, taux) VALUES
+('Heures sup 25%', 1, 2, 25),
+('Heures sup 50%', 3, 10, 50),
+('Heures sup 100%', 11, 9999, 100);
+
+INSERT INTO prime (nom, description, montant, type_prime) VALUES
+('Prime de rendement', 'Prime accordée pour performance exceptionnelle', 50000, 'mensuelle'),
+('Prime d\'anciennete', 'Prime en fonction des annees d\'anciennete', 20000, 'annuelle'),
+('Prime de Noel', 'Prime exceptionnelle de fin d\'annee', 100000, 'ponctuelle'),
+('Prime de presence', 'Prime pour presence parfaite sur le mois', 15000, 'mensuelle'),
+('Prime de projet', 'Prime pour reussite d\'un projet specifique', 30000, 'ponctuelle');
+
+
+INSERT INTO pourcentage_avance (pourcentage, date)
+VALUES
+(30, '2025-12-07'),
+(50, '2025-12-07');
